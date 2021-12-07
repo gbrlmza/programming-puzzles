eca06de6d5b8d51ab5fd65be808c0cd65c41215a
Gabriel Gonzalez <gbrlmza@gmail.com>
2021-12-07 10:38:45 -0300

    AOC 2021 Day 7 - Better solution using median and mean

--- adventofcode/2021/day07/main.go
+++ adventofcode/2021/day07/main.go
@@ -24,51 +24,34 @@ func main() {
 }
 
 func partOne(input []int) int {
+	var cost, median int
 	sort.Ints(input)
-	positions := make(map[int]int)
-	for i := input[0]; i <= input[len(input)-1]; i++ {
-		positions[i] = 0
-	}
-	for _, v := range input {
-		for p := range positions {
-			positions[p] += int(math.Abs(float64(v - p)))
-		}
-	}
-	cost := -1
-	for _, pCost := range positions {
-		if cost == -1 || pCost < cost {
-			cost = pCost
-		}
+	median = input[len(input)/2]
+	for _, n := range input {
+		cost += int(math.Abs(float64(n - median)))
 	}
 	return cost
 }
 
 func partTwo(input []int) int {
-	sort.Ints(input)
-	positions := make(map[int]int)
-	for i := input[0]; i <= input[len(input)-1]; i++ {
-		positions[i] = 0
+	var cost, mean int
+	for _, n := range input {
+		mean += n
 	}
+	mean = mean / len(input)
 
-	maxDistance := input[len(input)-1] - input[0]
+	sort.Ints(input)
+	maxDistance := int(math.Abs(float64(mean - input[0])))
+	if math.Abs(float64(mean-input[len(input)-1])) > float64(maxDistance) {
+		maxDistance = int(math.Abs(float64(mean - input[len(input)-1])))
+	}
 	distancesCost := make(map[int]int, maxDistance)
 	for i := 0; i <= maxDistance; i++ {
 		distancesCost[i] = distancesCost[i-1] + i
 	}
-
-	for _, v := range input {
-		for p := range positions {
-			positions[p] += distancesCost[int(math.Abs(float64(v-p)))]
-		}
+	for _, p := range input {
+		cost += distancesCost[int(math.Abs(float64(mean-p)))]
 	}
-
-	cost := -1
-	for _, pCost := range positions {
-		if cost == -1 || pCost < cost {
-			cost = pCost
-		}
-	}
-
 	return cost
 }
 
@@ -97,3 +80,52 @@ func getInput(path string) []int {
 
 	return output
 }
+
+// func partOne(input []int) int {
+// 	sort.Ints(input)
+// 	positions := make(map[int]int)
+// 	for i := input[0]; i <= input[len(input)-1]; i++ {
+// 		positions[i] = 0
+// 	}
+// 	for _, v := range input {
+// 		for p := range positions {
+// 			positions[p] += int(math.Abs(float64(v - p)))
+// 		}
+// 	}
+// 	cost := -1
+// 	for _, pCost := range positions {
+// 		if cost == -1 || pCost < cost {
+// 			cost = pCost
+// 		}
+// 	}
+// 	return cost
+// }
+
+// func partTwo(input []int) int {
+// 	sort.Ints(input)
+// 	positions := make(map[int]int)
+// 	for i := input[0]; i <= input[len(input)-1]; i++ {
+// 		positions[i] = 0
+// 	}
+
+// 	maxDistance := input[len(input)-1] - input[0]
+// 	distancesCost := make(map[int]int, maxDistance)
+// 	for i := 0; i <= maxDistance; i++ {
+// 		distancesCost[i] = distancesCost[i-1] + i
+// 	}
+
+// 	for _, v := range input {
+// 		for p := range positions {
+// 			positions[p] += distancesCost[int(math.Abs(float64(v-p)))]
+// 		}
+// 	}
+
+// 	cost := -1
+// 	for _, pCost := range positions {
+// 		if cost == -1 || pCost < cost {
+// 			cost = pCost
+// 		}
+// 	}
+
+// 	return cost
+// }
